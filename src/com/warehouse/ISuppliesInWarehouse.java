827c04578ce2dbf59f10cb075ee134952d9fd9e3
kambievbeslan <kambiev.b.m@gmail.com>
2021-07-12 14:55:47 +0300

    Переделал вывод, добавил работу с ценой и стоимостью. Материалы теперь равны если совпадает название и цена. fix1

---
+++ src/com/warehouse/ISuppliesInWarehouse.java
@@ -0,0 +1,13 @@
+package com.warehouse;
+
+public interface ISuppliesInWarehouse {
+    int AddEntry(SuppliesInWarehouse inEntryWarehouse);
+
+    void Delete(int Id);
+
+    String fullReport();
+
+    int getMaterialsCount();
+
+    boolean Contains(Material material);
+}
---
+++ src/com/warehouse/SuppliesInWarehouse.java
@@ -0,0 +1,4 @@
+package com.warehouse;
+/*Доставка материалов в склад*/
+public class SuppliesInWarehouse {
+}
